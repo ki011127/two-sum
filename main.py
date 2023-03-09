cc293ba859951551c82673c4cd62779b6cf0af39
SW-Skku <ki011127@naver.com>
2023-03-09 20:07:43 +0900

    initial version of main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+def twoSum(nums , target):
+
+    for i in range(len(nums)-1):
+        for j in range(i+1,len(nums)):
+            if nums[i]+nums[j]==target:
+                result = [i,j]
+                return result
